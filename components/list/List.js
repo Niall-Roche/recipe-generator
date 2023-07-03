97986bdfa4a2e1567350ea395ecb6702789c31f9
Niall Roche <nroche@showoff.ie>
2023-07-03 15:00:37 +0100

    Added styles to list component

--- components/list/List.js
+++ components/list/List.js
@@ -3,22 +3,27 @@ import Link from 'next/link'
 
 const List = ({items}) => {
   return (
-    <div className='grid grid-cols-5'>
-      {
-        items?.map(({strIngredient}, i) => (
-          <div className='flex flex-col items-center content-center h-auto max-w-full rounded-lg bg-stone-400 m-3 w-99 h-99 overflow-hidden' key={i}>
-            <Link href={`/recipes/${strIngredient}`}>
-              <h3 className='text-center'>{strIngredient}</h3>
-              <Image
-                src={`https://www.themealdb.com/images/ingredients/${strIngredient}-Small.png`}
-                height={100}
-                width={100}
-                alt={strIngredient}
-              />
-            </Link>
-          </div>
-        ))
-      }
+    <div className='container my-12 mx-auto px-4 md:px-12'>
+      <div className='flex flex-wrap -mx-1 lg:-mx-4'>
+        {
+          items?.map(({strIngredient}, i) => (
+            <div className='my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/6' key={i}>
+              <div className='group overflow-hidden rounded-lg lg:shadow-lg border sm:border-none border-gray-600 hover:shadow-none transition-shadow bg-neutral-100'>
+                <Link href={`/recipes/${strIngredient}`}>
+                  <h3 className='text-center font-bold'>{strIngredient}</h3>
+                  <Image
+                    className='block h-auto w-auto m-auto p-3 group-hover:scale-105 group-hover:rotate-6 transition-transform'
+                    src={`https://www.themealdb.com/images/ingredients/${strIngredient}-Small.png`}
+                    height={100}
+                    width={100}
+                    alt={strIngredient}
+                  />
+                </Link>
+              </div>
+            </div>
+          ))
+        }
+      </div>
     </div>
   )
 }
